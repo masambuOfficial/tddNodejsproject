004f4cfa48dbf04e35d85810056300bb2fc4c8d5
Masambu Emmanuel <jemasambu@gmail.com>
2022-02-04 23:09:52 +0300

    TDD Class project

---
+++ test/test.js
@@ -0,0 +1,22 @@
+// Test First TDD First Cycle
+// assertEqual(multiply(1, 1), 1);
+
+function multiply(a, b) {
+  return 1;
+}
+
+// Second Test Cycle
+// assertEqual(multiply(2, 2),4)
+
+multiply()
+
+// Third Test Cycle
+// assertEqual(3, 3), 9);
+
+multiply()
+
+// Fourth Test Cycle
+// assertEqual(4, 4), 16);
+
+// Fifth Test Cycle
+// assertEqual(23, 45), 23 * 45);
