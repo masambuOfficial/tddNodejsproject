1d9195105d68d40cd49c34082f0b568e24e2f391
Masambu Emmanuel <jemasambu@gmail.com>
2022-02-07 13:40:12 +0300

    Updated the Code with the write examples from Joshua

---
+++ test/multiply.js
@@ -0,0 +1,5 @@
+function multiply(a, b) {
+  const ans = a * b;
+  return ans;
+}
+module.exports = multiply;
--- test/test.js
+++ test/test.js
@@ -1,22 +1,31 @@
-// Test First TDD First Cycle
-// assertEqual(multiply(1, 1), 1);
+const assert = require('chai').assert;
+const multiply = require('../test/multiply');
 
-function multiply(a, b) {
-  return 1;
-}
+describe('My first TDD Code assignment', function(){
+    
+  it('1 times 1 = 1: Failed test', function(){
+      assert.equal(multiply(1,1), 0);
+  });
+  
+  //First Test
+  it('1 times 1 = 1', function(){
+      assert.equal(multiply(1,1), 1);
+  });
+  //Second Test
+  it('Second test', function(){
+      assert.equal(multiply(2,2), 4);
+  });
+  //Third Test
+  it('Third test', function(){
+      assert.equal(multiply(3,3), 9);
+  });
+  //Fourth test
+  it('Fourth test', function(){
+      assert.equal(multiply(4,4), 16);
+  });
+  //Fifth test
+  it('Fifth test', function(){
+    assert.equal(multiply(23,45), 23*45);
+  });
 
-// Second Test Cycle
-// assertEqual(multiply(2, 2),4)
-
-multiply()
-
-// Third Test Cycle
-// assertEqual(3, 3), 9);
-
-multiply()
-
-// Fourth Test Cycle
-// assertEqual(4, 4), 16);
-
-// Fifth Test Cycle
-// assertEqual(23, 45), 23 * 45);
+});
